197bd6dcd92020beb90940cc92ee54162bc2e325
Rambolo <iamrambolo@gmail.com>
2024-06-07 17:52:02 +0100

    Added window resizing to config & corrected displayed slot number in slot dropdown.

--- OHook/PaliaOverlay.h
+++ OHook/PaliaOverlay.h
@@ -965,6 +965,9 @@ public:
 
     // ==================================== //
     
+    int windowSizeX = 1450;
+    int windowSizeY = 950;
+
     int TeleportHotkey = VK_XBUTTON1;
     int defaultTeleportHotkey = VK_XBUTTON1;
     bool bWaitingForKeyPress = false;
@@ -1295,6 +1298,10 @@ public:
 
     // Quicksell Hotkeys
     bool bEnableQuicksellHotkeys = false;
+
+    // Window Size Default Values
+    int defaultWindowSizeX = 1450;
+    int defaultWindowSizeY = 950;
 
     // ESP Default Values
     bool defaultEnableESP = false;
